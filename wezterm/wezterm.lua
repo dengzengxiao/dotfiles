d02fc5e7071029c7d709ed24d6b9e5780434560e
dengzengxiao <dengzengxiao___@outlook.com>
2025-11-26 19:23:08 +0800

    Change font size in wezterm configuration
    Updated font size from 12 to 10 in wezterm configuration.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,7 +18,7 @@ config.win32_system_backdrop = "Acrylic"
 config.default_domain = "WSL:archlinux"
 
 config.font = wezterm.font_with_fallback({ "Maple Mono NF CN" })
-config.font_size = 12
+config.font_size = 10
 config.color_scheme = "Everforest Dark Medium (Gogh)"
 
 config.use_fancy_tab_bar = true
@@ -32,4 +32,5 @@ config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_rate = 650
 
 return config
+
 

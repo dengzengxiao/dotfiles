6cbc7498db4f42063f45d89bcf89fa8a1d4f752f
dengzengxiao <dengzengxiao___@outlook.com>
2026-08-20 10:17:41 +0800

    feat: add tiger code input method

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -5,6 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+vim.opt.expandtab = true
 
 vim.g.mkdp_refresh_slow = 1
 vim.g.mkdp_auto_close = 1
